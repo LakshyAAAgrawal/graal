710aeaaf9d37e031fa2726773d2c1078e73e6768
Marko Spasic <marko.spasic@oracle.com>
2022-10-05 12:13:16 +0200

    refactor util classes for Graph

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/image/NativeImageHeapGraph.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/image/NativeImageHeapGraph.java
@@ -1,100 +1,102 @@
 package com.oracle.svm.hosted.image;
 
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.IdentityHashMap;
+import java.util.Map;
 import java.util.Set;
 
 import com.oracle.svm.hosted.image.NativeImageHeap.ObjectInfo;
 
-interface Graph<Node> {
-    void addNode(Node a);
-    void connect(Node a, Node b);
-    Collection<Node> getNeighbours(Node a);
-    Collection<Collection<Node>> computeConnectedComponents();
-    long getNumberOfNodes();
-    long getNumberOfEdges();
-}
-
-class DirectedGraph<Node> implements Graph<Node> {
-    private final IdentityHashMap<Node, Set<Node>> adjList = new IdentityHashMap<>();
-    long numberOfEdges = 0;
-
-    @Override
-    public void addNode(Node a) {
-        adjList.computeIfAbsent(a, node -> Collections.newSetFromMap(new IdentityHashMap<>()));
+abstract class AbstractGraph<Node> {
+    protected final IdentityHashMap<Node, Set<Node>> adjList = new IdentityHashMap<>();
+    protected long numberOfEdges = 0;
+    public Set<Node> addNode(Node a) {
+        return adjList.computeIfAbsent(a, node -> Collections.newSetFromMap(new IdentityHashMap<>()));
     }
-
-    @Override
     public void connect(Node a, Node b) {
-        Set<Node> adjNodes = adjList.computeIfAbsent(a, node -> Collections.newSetFromMap(new IdentityHashMap<>()));
+        Set<Node> adjNodes = addNode(a);
         numberOfEdges += adjNodes.add(b) ? 1 : 0;
-        adjList.computeIfAbsent(b, node -> Collections.newSetFromMap(new IdentityHashMap<>()));
+        addNode(b);
     }
-
-    @Override
     public Collection<Node> getNeighbours(Node a) {
         return adjList.get(a);
     }
-
-    @Override
-    public Collection<Collection<Node>> computeConnectedComponents() {
-        return null;
-    }
-
-    @Override
+    public abstract Collection<Collection<Node>> computeConnectedComponents();
     public long getNumberOfNodes() {
         return adjList.size();
     }
-
-    @Override
     public long getNumberOfEdges() {
         return numberOfEdges;
     }
+
+    protected abstract void dumpGraphBegin(PrintStream out);
+    protected abstract void dumpEdge(PrintStream out, Node a, Node b);
+    protected abstract void dumpGraphEnd(PrintStream out);
+
+    public void dumpGraph(PrintStream out) {
+        dumpGraphBegin(out);
+        StringBuilder buffer = new StringBuilder();
+        for (Map.Entry<Node, Set<Node>> nodeSetEntry : adjList.entrySet()) {
+            Node root = nodeSetEntry.getKey();
+            Set<Node> neighbours = nodeSetEntry.getValue();
+            for (Node neighbour : neighbours) {
+                dumpEdge(out, root, neighbour);
+            }
+        }
+        dumpGraphEnd(out);
+    }
 }
 
-class UndirectedGraph<Node> implements Graph<Node> {
-    private final IdentityHashMap<Node, Set<Node>> adjList = new IdentityHashMap<>();
-    long numberOfEdges = 0;
-
-    @Override
-    public void addNode(Node a) {
-        adjList.computeIfAbsent(a, node -> Collections.newSetFromMap(new IdentityHashMap<>()));
-    }
-
-    @Override
-    public void connect(Node a, Node b) {
-        Set<Node> adjNodes = adjList.computeIfAbsent(a, node -> Collections.newSetFromMap(new IdentityHashMap<>()));
-        numberOfEdges += adjNodes.add(b) ? 1 : 0;
-        adjNodes = adjList.computeIfAbsent(b, node -> Collections.newSetFromMap(new IdentityHashMap<>()));
-        adjNodes.add(a);
-    }
-
-    @Override
-    public Collection<Node> getNeighbours(Node a) {
-        return adjList.get(a);
-    }
-
+class DirectedGraph<Node> extends AbstractGraph<Node> {
     @Override
     public Collection<Collection<Node>> computeConnectedComponents() {
         return null;
     }
 
     @Override
-    public long getNumberOfNodes() {
-        return 0;
+    protected void dumpGraphBegin(PrintStream out) {
+        out.println("digraph G0 {");
     }
 
     @Override
-    public long getNumberOfEdges() {
-        return numberOfEdges;
+    protected void dumpEdge(PrintStream out, Node a, Node b) {
+        out.printf("%s -> %s\n", a.toString(), b.toString());
+    }
+
+    @Override
+    protected void dumpGraphEnd(PrintStream out) {
+        out.println("}");
+    }
+}
+
+class UndirectedGraph<Node> extends AbstractGraph<Node> {
+    @Override
+    public Collection<Collection<Node>> computeConnectedComponents() {
+        return null;
+    }
+
+    @Override
+    protected void dumpGraphBegin(PrintStream out) {
+        out.println("graph G0 {");
+    }
+
+    @Override
+    protected void dumpEdge(PrintStream out, Node a, Node b) {
+        out.printf("%s -- %s\n", a.toString(), b.toString());
+    }
+
+    @Override
+    protected void dumpGraphEnd(PrintStream out) {
+        out.println("}");
     }
 }
 
 public class NativeImageHeapGraph {
-    private final UndirectedGraph<Object> graph = new UndirectedGraph<>();
+    private final AbstractGraph<Object> graph = new UndirectedGraph<>();
     private final HashMap<String, Set<Object>> methodToConstant = new HashMap<>();
 
     private static ArrayList<ObjectInfo> getAllReferencesToObjectInHeap(ObjectInfo objectInfo) {
@@ -112,6 +114,12 @@ public class NativeImageHeapGraph {
 
     public NativeImageHeapGraph(NativeImageHeap heap) {
 //        ArrayList<Pair<String, Object>> allReasons = new ArrayList<>();
+        graph.connect("A", "B");
+        graph.connect("B", "C");
+        graph.connect("B", "D");
+        graph.connect("C", "D");
+        graph.connect("D", "A");
+        graph.dumpGraph(System.out);
         for (ObjectInfo objectInfo : heap.getObjects()) {
             Object object = objectInfo.getObject();
             assert object != null;
