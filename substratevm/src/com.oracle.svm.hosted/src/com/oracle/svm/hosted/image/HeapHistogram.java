a19fabb8e9eebaf240450668a644bddb9d114146
Marko Spasic <marko.spasic@oracle.com>
2022-11-06 18:18:25 +0100

    Enable printing HeapHistogram to an arbitrary PrintWritter

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/image/HeapHistogram.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/image/HeapHistogram.java
@@ -24,6 +24,8 @@
  */
 package com.oracle.svm.hosted.image;
 
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -39,6 +41,7 @@ public class HeapHistogram {
     protected static boolean PrintStrings = false;
 
     private final Map<HostedClass, HistogramEntry> data = new HashMap<>();
+    private final PrintWriter out;
 
     static class HistogramEntry {
         protected final HostedClass clazz;
@@ -48,6 +51,14 @@ public class HeapHistogram {
         HistogramEntry(HostedClass clazz) {
             this.clazz = clazz;
         }
+    }
+
+    public HeapHistogram() {
+        this.out = new PrintWriter(System.out);
+    }
+
+    public HeapHistogram(PrintWriter out) {
+        this.out = out;
     }
 
     private static final Comparator<HistogramEntry> SIZE_COMPARATOR = (o1, o2) -> {
@@ -80,15 +91,15 @@ public class HeapHistogram {
             String reason = String.valueOf(objectInfo.getMainReason());
             String value = ((String) objectInfo.getObject()).replace("\n", "");
             if (!reason.startsWith("com.oracle.svm.core.hub.DynamicHub")) {
-                System.out.format("%120s ::: %s\n", value, reason);
+                out.format("%120s ::: %s\n", value, reason);
             }
         }
     }
 
     public void printHeadings(final String title) {
         assert NativeImageOptions.PrintHeapHistogram.getValue();
-        System.out.format("\n%s\n", title);
-        System.out.format(headerFormat, "Count", "Size", "Size%", "Cum%", "Class");
+        out.format("\n%s\n", title);
+        out.format(headerFormat, "Count", "Size", "Size%", "Cum%", "Class");
     }
 
     public void print() {
@@ -101,7 +112,7 @@ public class HeapHistogram {
         long printedSize = 0;
         for (HistogramEntry entry : entries) {
             printedSize += entry.size;
-            System.out.format(entryFormat, entry.count, entry.size, entry.size * 100d / totalSize, printedSize * 100d / totalSize, entry.clazz.toJavaName());
+            out.format(entryFormat, entry.count, entry.size, entry.size * 100d / totalSize, printedSize * 100d / totalSize, entry.clazz.toJavaName());
         }
     }
 
